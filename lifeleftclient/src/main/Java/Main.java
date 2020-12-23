ec5b08c8e7ea05a6e2878008a139d2780ea02622
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-12-23 15:20:09 +0100

    Cimmit lifeleftclient

---
+++ lifeleftclient/src/main/Java/Main.java
@@ -0,0 +1,15 @@
+import generated.leftlife.LifeLeft;
+import generated.leftlife.LifeLeftService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LifeLeft lifeleft = new LifeLeft();
+
+        LifeLeftService lifeleftsrv = lifeleft.getLifeLeftServicePort();
+
+        String resultat = lifeleftsrv.anneeRestantesAVivre("john", "homme", 1980);
+
+        System.out.println(resultat);
+
+    } }
